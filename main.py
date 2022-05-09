f9e7dc64ee231ce0b2d440f12feb561ca0191018
Bruce Garcia <bruce.garcia@adtran.com>
2022-05-08 19:56:46 -0700

    First commit

---
+++ main.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+# May 6, 2022
+
+
+# List
+userList = ["bob", "bill", "bruce"]
+
+# Tuple
+phoneNumbers = (9710001234, 9710002222)
+
+# Strings
+message = "hello friend"
+
+# Set
+someSet = {10, 2, "a", "d"}
+
+
+def compare(x, y):
+    """Comparing values"""
+    if x == y:
+        return True
+    else:
+        return False
+
+
+print(compare.__doc__)
+print(compare(7, 7))
+print(userList)
+print(phoneNumbers)
+print(soii
+
+
+
+
+
